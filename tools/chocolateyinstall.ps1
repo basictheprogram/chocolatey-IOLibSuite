0850ae2492db26ad461979469bb82ff911045917
Bob Tanner <tanner@real-time.com>
2019-05-29 15:51:37 -0500

    Private package only, just store and use the MSI for the installation.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,24 +2,16 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $workSpace    = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
-$url          = 'https://download.keysight.com/AS/IOLibSuite_18_1_24130.exe?id=3027604&dl=1&authparam=1559100994_fd50d64e8f7f4e15d729ef26f16a35ff'
-$fileLocation = 'N:\gold-images\keysight-iolib\IOLibSuite_18_1_24130.exe'
-
-$extractArgs = @{
-  Statements     = '/s /x /b"'+ $workSpace +'" /v"/qn"'
-  ExetoRun       = $fileLocation
-  checksum       = '6de849c7df31b71d4b01136ec1e66f75517a7f21cfbd23b0d6a8e26d450896c8'
-  checksumType   = 'sha256'
-  validExitCodes = @(-3, 0, 1603)
-}
-$exitCode = Start-ChocolateyProcessAsAdmin @extractArgs
+$fileLocation = 'N:\gold-images\keysight-iolib\Keysight IO Libraries Suite 2019.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $workSpace
   fileType       = 'MSI'
-  fileFullPath   = Join-Path $workSpace "Keysight IO Libraries Suite 2019.msi"
+  fileFullPath   = $fileLocation
   softwareName   = 'IOLibSuite*'
+  checksum       = 'c318426c510b092e7ba4ceed5e1cd90e12f456a3adfec060f190acb7a2503912'
+  checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1 APIINSTALL=0 LICENSETYPE=0"
   validExitCodes = @(0, 3010, 1641)
 }
